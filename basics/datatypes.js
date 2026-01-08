de7b8f53b14f99bef6ed3c7af73d325d8381c6f5
ankur7033 <ankurkr7033@gmail.com>
2026-01-08 13:29:38 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+let a = 2345      //number
+let b = 4567890987654       //bigint
+let c ="ram aam khata hai "      //string 
+let d = true       //boolean
+let e = null       //null
+let f;             //undefined
+
+console.table([typeof a,typeof b,typeof c,typeof d,typeof e,typeof f])
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// console.log("hello");
+
+const a = 4;
+// a =98;    value of const variable can not be changed
+
+let b = "ankur"
+var c = "banglore"//it having problems in scope so we don not use it nowadays
+d = "java script"
+
+
+b = "indresh";
+
+console.log(a)
+console.table([a,b,c,d])
