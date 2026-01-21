4c062533cec75201d945e8044d4b4a380e598d6c
ankur7033 <ankurkr7033@gmail.com>
2026-01-21 08:40:47 +0000

    objects

---
+++ Objects/01.js
@@ -0,0 +1,37 @@
+const mySym = Symbol("key1")
+
+const obj = {
+    name:"Ankur",
+    "full name":"Ankur Kumar",
+    age:22,
+    location:"Bengaluru",
+    email:"ankur@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"],
+    [mySym]:"myKey1"
+}
+
+// console.log(obj.name)
+// console.log(obj["name"])
+// console.log(obj["full name"])
+// console.log(obj[mySym])
+
+obj.name = "Mohan"
+// console.log(obj.name);
+
+// Object.freeze(obj)
+obj.name = "Sohan"
+// console.log(obj.name); // name will not be changed because obj is freesed
+
+obj.greeting = function(){
+    console.log("hello")
+}
+
+// console.log(obj)
+console.log(obj.greeting())
+
+obj.greeting2 = function(){
+    console.log(`My name is ${this.name}`) 
+}
+console.log(obj.greeting2())
+console.log(obj)
