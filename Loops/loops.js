634f1855274946e0835dacc4773bf9e8355e5a6c
ankur7033 <ankurkr7033@gmail.com>
2026-02-17 10:45:51 +0000

    loop on maps and objects.

--- Loops/loops.js
+++ Loops/loops.js
@@ -9,12 +9,27 @@ for (const s of str){
     // console.log(s)
 }
 
+const map = new Map()
+map.set(1,'Indresh')
+map.set(2,'Aman')
+map.set(3,'Ankur')
+for (const[key,value]of map){
+    console.log(key+' => '+value)
+}
+console.log(map)
+
 const obj = {
     id:"01",
     name:"Sarthak",
     dept :"AEIE"
 }
 
-for([key,value] in obj){
-    console.log(o)
+for(const key in obj){
+    console.log(key +' and value is '+obj[key])
+}
+
+// using 'For in ' in array
+const array = [2,3,4,5,6,7,8,8,9]
+for(const element in array){
+    console.log(element)
 }
