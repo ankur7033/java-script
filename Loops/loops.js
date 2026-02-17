cbd425eb3e549efcd717f5f0f485c7e37cd8278a
ankur7033 <ankurkr7033@gmail.com>
2026-02-17 08:49:44 +0000

    loops

---
+++ Loops/loops.js
@@ -0,0 +1,20 @@
+const arr = [2,3,4,45,6]
+
+for(const a of arr){
+    // console.log(a)
+}
+
+const str = 'Hello world!'
+for (const s of str){
+    // console.log(s)
+}
+
+const obj = {
+    id:"01",
+    name:"Sarthak",
+    dept :"AEIE"
+}
+
+for([key,value] in obj){
+    console.log(o)
+}
