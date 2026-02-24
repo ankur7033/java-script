dc75885972705eaadb5a61ee990e805f25485c8e
ankur7033 <ankurkr7033@gmail.com>
2026-02-24 11:33:47 +0000

    reduce

--- Loops/reduce.js
+++ Loops/reduce.js
@@ -9,3 +9,31 @@ let sum = arr1.reduce(
 //OR
  sum = arr1.reduce((acc,curr)=>curr+acc,init);
 console.log(sum)
+
+const myTotal = arr1.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
